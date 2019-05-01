ae42dbba17e19dfe1aa5f55696144b2a45753b18
Boris Rasin <boris@pointx.org>
2019-05-02 02:19:32 +0300

    use vs2019 by default

--- project/vs_java.cmd
+++ project/vs_java.cmd
@@ -1,1 +1,1 @@
-@cmd /c "mkdir build\vs_java & cd build\vs_java & cmake -Dscapix_bridge=java -Dscapix_platform=java -Dscapix_java_api=android-28 -G "Visual Studio 15 2017" -T host=x64 -A x64 -DVCPKG_TARGET_TRIPLET=x64-windows-static -DCMAKE_TOOLCHAIN_FILE=e:/library/build/vcpkg/scripts/buildsystems/vcpkg.cmake ../.."
+@cmd /c "mkdir build\vs_java & cd build\vs_java & cmake -Dscapix_bridge=java -Dscapix_platform=java -Dscapix_java_api=jdk-11.0.2 -G "Visual Studio 16 2019" -T host=x64 -A x64 ../.."
