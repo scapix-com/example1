a03eacdff1942ba04b57b14dbb9aa06022f5053d
Boris Rasin <boris@pointx.org>
2019-05-11 22:41:46 +0300

    update python ui sample

--- ui/python/run.py
+++ ui/python/run.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 
 import os, sys
+sys.path.append(os.path.join(os.path.dirname(__file__), '../../project/build/vs_python/Debug'))
 sys.path.append(os.path.join(os.path.dirname(__file__), '../../project/build/vs_python/Release'))
 sys.path.append(os.path.join(os.path.dirname(__file__), '../../project/build/xcode_mac_python/Debug'))
+sys.path.append(os.path.join(os.path.dirname(__file__), '../../project/build/xcode_mac_python/Release'))
 
 import chatlib
 
