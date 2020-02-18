bcbaaaa4942d357c4fe59f8b3b3f6deae55053ce
Boris Rasin <boris@pointx.org>
2020-02-18 18:15:37 +0200

    emscripeten: use new "upstream" backend instead of old "fastcomp" backend

--- project/emscripten.cmd
+++ project/emscripten.cmd
@@ -1,4 +1,4 @@
+@rem adjust path to emsdk
 call c:\library\dev\emsdk\emsdk_env.bat
-rem cmake -B build/emscripten -DSCAPIX_BRIDGE=js -DCMAKE_TOOLCHAIN_FILE=c:/library/dev/polly/emscripten-cxx17.cmake
-cmake -B build/emscripten -DSCAPIX_BRIDGE=js -DCMAKE_TOOLCHAIN_FILE=%EMSDK%/fastcomp/emscripten/cmake/Modules/Platform/Emscripten.cmake
+cmake -B build/emscripten -DSCAPIX_BRIDGE=js -DCMAKE_TOOLCHAIN_FILE=%EMSDK%/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake
 cmake --build build/emscripten
--- project/emscripten.sh
+++ project/emscripten.sh
@@ -1,4 +1,4 @@
+# adjust path to emsdk
 source ~/Source/emsdk/emsdk_env.sh
-#cmake -B build/emscripten -DSCAPIX_BRIDGE=js -DCMAKE_TOOLCHAIN_FILE=~/Source/polly/emscripten-cxx17.cmake
-cmake -B build/emscripten -DSCAPIX_BRIDGE=js -DCMAKE_TOOLCHAIN_FILE=$EMSDK/fastcomp/emscripten/cmake/Modules/Platform/Emscripten.cmake
+cmake -B build/emscripten -DSCAPIX_BRIDGE=js -DCMAKE_TOOLCHAIN_FILE=$EMSDK/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake
 cmake --build build/emscripten
