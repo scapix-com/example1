1ff388bbb7f79bc87e515363c52086811582b0b6
Boris Rasin <boris@pointx.org>
2020-03-21 03:56:30 +0200

    use ninja for emscripten build on windows

--- project/emscripten.cmd
+++ project/emscripten.cmd
@@ -1,4 +1,4 @@
 @rem adjust path to emsdk
 call c:\library\dev\emsdk\emsdk_env.bat
-cmake -B build/emscripten -DSCAPIX_BRIDGE=js -DCMAKE_TOOLCHAIN_FILE=%EMSDK%/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake
+cmake -B build/emscripten -G Ninja -DSCAPIX_BRIDGE=js -DCMAKE_TOOLCHAIN_FILE=%EMSDK%/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake
 cmake --build build/emscripten
