170d24940bd9c22ceade1036009ad026ba61976a
Boris Rasin <boris@pointx.org>
2021-05-30 16:31:52 +0300

    add `setlocal` to cmd script

--- emscripten.cmd
+++ emscripten.cmd
@@ -1,4 +1,6 @@
 @rem adjust path to emsdk
+@echo off
+setlocal
 call c:\library\dev\emsdk\emsdk_env.bat
 cmake -B build/emscripten -G Ninja -DSCAPIX_BRIDGE=js -DCMAKE_TOOLCHAIN_FILE=%EMSDK%/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake
 cmake --build build/emscripten
