9e7b54d8698d0a97630e0568ceb171dc7401955f
Boris Rasin <boris@pointx.org>
2021-11-12 01:52:00 +0200

    improve EMSDK environment variable detection

--- emscripten.sh
+++ emscripten.sh
@@ -1,4 +1,11 @@
-# adjust path to emsdk
-source ~/Source/emsdk/emsdk_env.sh
+if [ -z "$EMSDK" ]; then
+  if [ -f ~/Source/emsdk/emsdk_env.sh ]; then
+    source ~/Source/emsdk/emsdk_env.sh
+  else
+    echo "error: no EMSDK environment variable, run emsdk/emsdk_env.sh"
+    exit 1
+  fi
+fi
+
 cmake -B build/emscripten -DSCAPIX_BRIDGE=js -DCMAKE_TOOLCHAIN_FILE=$EMSDK/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake
 cmake --build build/emscripten
