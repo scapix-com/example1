119b38f98a103deb6081a9c5ec5404a3f31cade9
Boris Rasin <boris@pointx.org>
2021-11-17 15:02:11 +0200

    improve EMSDK environment variable detection (ui)

--- ui/js/serve.cmd
+++ ui/js/serve.cmd
@@ -1,3 +1,13 @@
-@rem adjust path to emsdk
-call c:\library\dev\emsdk\emsdk_env.bat
+@echo off
+
+IF NOT DEFINED EMSDK (
+  IF EXIST c:\library\dev\emsdk\emsdk_env.bat (
+    setlocal
+    call c:\library\dev\emsdk\emsdk_env.bat
+  ) ELSE (
+    echo "error: no EMSDK environment variable, run emsdk\emsdk_env.bat"
+    exit /b 1
+  )
+)
+
 %EMSDK%\upstream\emscripten\emrun --serve_root ../.. test.html
--- ui/js/serve.sh
+++ ui/js/serve.sh
@@ -1,2 +1,10 @@
-source ~/Source/emsdk/emsdk_env.sh
+if [ -z "$EMSDK" ]; then
+  if [ -f ~/Source/emsdk/emsdk_env.sh ]; then
+    source ~/Source/emsdk/emsdk_env.sh
+  else
+    echo "error: no EMSDK environment variable, run emsdk/emsdk_env.sh"
+    exit 1
+  fi
+fi
+
 emrun --serve_root ../.. test.html
