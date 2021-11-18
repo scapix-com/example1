308b53fb478e411d76700ae1357c7ba8cd27149f
Boris Rasin <boris@pointx.org>
2021-11-18 11:57:20 +0200

    remove no longer needed `export JAVA_HOME` (Mac)

--- xcode_mac_java.sh
+++ xcode_mac_java.sh
@@ -1,3 +1,2 @@
-export JAVA_HOME=`/usr/libexec/java_home`
 cmake -B build/xcode_mac_java -G "Xcode" -DSCAPIX_BRIDGE=java -DSCAPIX_PLATFORM=java -DSCAPIX_JAVA_API=jdk-11.0.2
 cmake --build build/xcode_mac_java
